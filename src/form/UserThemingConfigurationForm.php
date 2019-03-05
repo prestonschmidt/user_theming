2afc41e088349d13a8399af4afc1bf0e729e4bde
Preston Schmidt <phoebuslife@gmail.com>
2019-03-05 07:29:33 -0800

    adding new user fields. Updating image styles. Updating views hoks

--- src/form/UserThemingConfigurationForm.php
+++ src/form/UserThemingConfigurationForm.php
@@ -6,96 +6,97 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Defines a form that configures size and shape settings of the user theming module.
+ * Defines a form for the user theming module.
  */
 class UserThemingConfigurationForm extends ConfigFormBase {
 
-  /**
-   * {@inheritdoc}
-   */
-    public function getFormId() {
-        return 'user_theming_settings';
-    }
+ /**
+  * {@inheritdoc}
+  */
+  public function getFormId() {
+    return 'user_theming_settings';
+  }
 
-  /**
-   * {@inheritdoc}
-   */
-    protected function getEditableConfigNames() {
-        return [
-            'user_theming.user_theming_settings',
-        ];
-    }
+ /**
+  * {@inheritdoc}
+  */
+  protected function getEditableConfigNames() {
+    return [
+      'user_theming.user_theming_settings',
+    ];
+  }
 
-  /**
-   * {@inheritdoc}
-   */
-    public function buildForm(array $form, FormStateInterface $form_state) {
-        
-        $config = $this->config('user_theming.user_theming_settings');
-        
-        $yesnoOptions = array(
-            'yes' => t('Yes'),
-            'no' => t('No'),
-        );
-        
-        $form['user_theming_heading'] = [
-            '#type' => 'item',
-            '#markup' => t('<h2>Available Configuration Options</h2>'),
-        ];
-        
-        $form['show_active_session_mark_wrapper'] = array(
-            '#type' => 'fieldset',
-            '#weight' => 5,
-            '#attributes' => array(
-                'class' => array(
-                    'show-active-session-mark',
-                ),
-            ),
-        );
-        
-        $form['show_active_session_mark_wrapper']['show_active_session_mark'] = array(
-            '#type' => 'radios',
-            '#title' => t('Show active user mark?'),
-            '#required' => true,
-            '#options' => $yesnoOptions,
-            '#description' => t('If set to "Yes", this will show an active session mark. '),
-            '#default_value' => $config->get('show_active_session_mark') ? : 'yes',
-        );
-        
-        $form['form_details'] = [
-            '#type' => 'item',
-            '#weight' => 10,
-            '#markup' => t('
-<h3>This module provides:</h3>
-<ul>
-<li><strong>Display Modes:</strong> Full, Compact, Token</li>
-<li><strong>Template Files:</strong> user--full.html.twig, user--compact.html.twig, user--token.html.twig</li>
-<li><strong>Views:</strong> users_compact_views_block, users_token_views_block</li>
-<li><strong>Fields:</strong> field_user_first_name, field_user_last_name, field_user_title, user_picture</li>
-</ul>
-            '),
-        ];
-        
-        $form['form_additional_info'] = [
-            '#type' => 'item',
-            '#weight' => 50,
-            '#markup' => t('
-            <p>It also provides optional styling for the various user displays. Built on Less, CSS Grid, and mobile responsive design.</p><p>&nbsp;</p>
-            '),
-        ];
-        
-        return parent::buildForm($form, $form_state);
-    }
+ /**
+  * {@inheritdoc}
+  */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    
+    $config = $this->config('user_theming.user_theming_settings');
+    
+    $yesnoOptions = [
+      'yes' => t('Yes'),
+      'no' => t('No'),
+    ];
+    
+    $form['user_theming_heading'] = [
+      '#type' => 'item',
+      '#markup' => t('<h2>Available Configuration Options</h2>'),
+    ];
+    
+    $form['show_active_session_mark_wrapper'] = [
+      '#type' => 'fieldset',
+      '#weight' => 5,
+      '#attributes' => [
+        'class' => [
+          'show-active-session-mark',
+        ],
+      ],
+    ];
+    
+    $form['show_active_session_mark_wrapper']['show_active_session_mark'] = [
+      '#type' => 'radios',
+      '#title' => t('Show active user mark?'),
+      '#required' => true,
+      '#options' => $yesnoOptions,
+      '#description' => t('If set to "Yes", this will show an active session mark.'),
+      '#default_value' => $config->get('show_active_session_mark') ?: 'yes',
+    ];
 
-  /**
-   * {@inheritdoc}
-   */
-    public function submitForm(array &$form, FormStateInterface $form_state) {
-        $values = $form_state->getValues();
-        
-        $this->config('user_theming.user_theming_settings')->set('show_active_session_mark', $values['show_active_session_mark'])->save();
-        parent::submitForm($form, $form_state);
-        
-    }
+    $form['use_module_provided_css_wrapper'] = [
+      '#type' => 'fieldset',
+      '#weight' => 5,
+      '#attributes' => [
+        'class' => [
+          'show-active-session-mark',
+        ],
+      ],
+    ];
+    
+    $form['use_module_provided_css_wrapper']['use_module_provided_css'] = [
+      '#type' => 'radios',
+      '#title' => t('Use module provided CSS?'),
+      '#required' => true,
+      '#options' => $yesnoOptions,
+      '#description' => t('If set to "Yes", this will use the module provided CSS.'),
+      '#default_value' => $config->get('use_module_provided_css') ?: 'yes',
+    ];
+    
+    return parent::buildForm($form, $form_state);
+  }
+
+ /**
+  * {@inheritdoc}
+  */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    
+    $this->configFactory->getEditable('user_theming.user_theming_settings')
+     ->set('show_active_session_mark', $values['show_active_session_mark'])
+     ->set('use_module_provided_css', $values['use_module_provided_css'])
+     ->save();
+     
+    parent::submitForm($form, $form_state);
+    
+  }
 
 }
